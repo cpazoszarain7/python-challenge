338db8f15b598511e17b979cbd4b227ba8a64845
Carlos Pazos <cpazoszarain@gmail.com>
2021-01-06 19:07:27 -0500

    added code to read .csv file

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,8 +1,22 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jan  3 16:27:58 2021
 
-@author: carlospazos
-"""
-#Main.py for PyBank Challenge
+#Import supporting Python modules
+import csv
+import os
+
+#Defnie relative path of .csv file
+csvpath = os.path.join('Resources','budget_data.csv')
+
+#Initialize lists to store contents of .csv
+Date=[]
+Profit_Loss=[]
+
+#Open file and read contents
+with open(csvpath) as csvfile:
+    
+    #Create csv reader object
+    csvreader = csv.reader(csvfile,delimiter=',')
+    
+    #Read each row of the file and store in respective lists
+    for row in csvreader:
+        Date.append(row[0])
+        Profit_Loss.append(row[1])
